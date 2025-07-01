6051d1a4b0f1253058a9bac2554dfb0d1fb9abfd
cefqrn <cefqrn@gmail.com>
2025-07-01 00:50:02 -0400

    sealed Code and made codes records

--- src/gif/lzw/Lzw.java
+++ src/gif/lzw/Lzw.java
@@ -1,7 +1,8 @@
 package gif.lzw;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import gif.data.exception.OutOfBounds;
 import gif.data.exception.ParseException;
@@ -9,7 +10,7 @@ import gif.data.exception.ParseException;
 public class Lzw {
   public static final int MAXIMUM_CODE_SIZE = 12;
 
-  public static ArrayList<Integer> decode(BitStream stream, int minimumCodeSize) throws ParseException {
+  public static List<Integer> decode(BitStream stream, int minimumCodeSize) throws ParseException {
     var codes = getInitialCodes(minimumCodeSize);
     var codeSize = minimumCodeSize + 1;
 
@@ -33,7 +34,7 @@ public class Lzw {
       }
 
       if (code == Code.END_OF_INFORMATION)
-        break;
+        return result;
 
       if (code == Code.CLEAR) {
         codes = getInitialCodes(minimumCodeSize);
@@ -45,28 +46,26 @@ public class Lzw {
       }
 
       var pattern = (Code.Pattern)code;
-      for (var value : pattern.getValues())
-        result.add(value);
+
+      result.addAll(pattern.values);
 
       if (reachedLastPattern)
         continue;
 
       if (previousPattern != null) {
-        codes.add(previousPattern.with(pattern.getInitialValue()));
+        codes.add(previousPattern.with(pattern.values.getFirst()));
         if (isLastPatternForCurrentSize)
           codeSize++;
       }
 
       previousPattern = pattern;
     }
-
-    return result;
   }
 
-  private static ArrayList<Code> getInitialCodes(int minimumCodeSize) {
+  private static List<Code> getInitialCodes(int minimumCodeSize) {
     var codes = new ArrayList<Code>((1 << minimumCodeSize) + 2);
     for (var i=0; i < (1 << minimumCodeSize); ++i)
-      codes.add(new Code.Pattern(new int[]{i}));
+      codes.add(new Code.Pattern(List.of(i)));
 
     codes.add(Code.CLEAR);
     codes.add(Code.END_OF_INFORMATION);
@@ -74,37 +73,23 @@ public class Lzw {
     return codes;
   }
 
-  private static class Code {
-    public static final ClearCode CLEAR = new ClearCode();
-    public static final EndOfInformationCode END_OF_INFORMATION = new EndOfInformationCode();
+  private static sealed interface Code {
+    static final ClearCode CLEAR = new ClearCode();
+    static final EndOfInformationCode END_OF_INFORMATION = new EndOfInformationCode();
 
-    private static class ClearCode extends Code {}
-    private static class EndOfInformationCode extends Code {}
+    record ClearCode()                   implements Code {}
+    record EndOfInformationCode()        implements Code {}
 
-    public static class Pattern extends Code {
-      private final int values[];
-
-      public Pattern(int values[]) {
-        this.values = values.clone();
-      }
-
-      public int[] getValues() {
-        return values.clone();
-      }
-
-      public int getInitialValue() {
-        return values[0];
-      }
-
+    record Pattern(List<Integer> values) implements Code {
       public Pattern with(int value) {
-        var newValues = Arrays.copyOf(values, values.length + 1);
-        newValues[values.length] = value;
+        var newValues = new ArrayList<>(values);
+        newValues.add(value);
 
-        return new Pattern(newValues);
+        return new Pattern(Collections.unmodifiableList(newValues));
       }
 
       public Pattern extended() {
-        return this.with(getInitialValue());
+        return this.with(values.getFirst());
       }
     }
   }
